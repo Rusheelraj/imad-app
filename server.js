40d4b8255fa4384e3386497254fc69b7d7255928
Rusheel Raj Panakkadan <30828807+Rusheelraj@users.noreply.github.com>
2017-08-10 20:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article-one | Rusheel Panakkadan',
+    heading:'Article-one',
+    date:' August 9,Wednesday,2017.',
+    content:`
+            <p> 
+            I am Rusheel Panakkadan studying Computer Science Engg in Veltech University.I m glad you are reading my article :)
+            </p>
+            <p>
+            I m interested in Networks and Security,and ofcourse hacking. hehe :)
+            
+            </p>`
+};
+function createTemplate (data){
+    var title = data.title;
+    var date =data.date;
+    var heading=data.heading;
+    var content=data.content;
+ var htmlTemplate = `
+    <html>
+    <head>
+        <title> ${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+        
+   
+        
+    </head>
+    
+    <body>
+        
+        <div class="container">
+        <div>
+            
+            <a href="/">Home</a>
+        </div>
+         <hr/>
+    
+        <h3>
+        ${heading}
+        </h3>
+        
+        <div>
+         ${date}
+        </div>
+    
+        <div> 
+        
+        ${content} 
+        </div>
+        </div>
+    </body>
+</html>
+
+    
+`;
+}
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
